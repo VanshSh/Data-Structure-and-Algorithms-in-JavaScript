5e9b878ba3a2b01eef6cd6133cd78e253cdc17a6
VanshSh <vanshsharma9354@gmail.com>
2023-02-19 19:12:36 +0530

    feat: :tada: Learning DSA
    Learning DSA from youtube and github repository

---
+++ binary-search.js
@@ -0,0 +1,40 @@
+// console.log('Binary Search')
+
+// function bubbleSortInc(arr, target) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (let i = 0; i < arr.length - 1; i++) {
+//             if (arr[i] > arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped)
+//     return binarySearch(arr, target)
+// }
+
+// function binarySearch(arr, target) {
+//     let min = 0
+//     let max = arr.length - 1
+//     while (min <= max) {
+//         let middle = Math.floor((min + max) / 2)
+//         if (arr[middle] === target) {
+//             return middle
+//         }
+//         if (arr[middle] < target) {
+//             min = middle + 1
+//         }
+//         if (arr[middle] > target) {
+//             max = middle - 1
+//         }
+//     }
+//     return -1
+// }
+
+// const arr = [5, 3, 8, -2, 1, 4, -6, 9, 7, 0, 2]
+
+// console.log(bubbleSortInc(arr, 2))
+// console.log(arr)
---
+++ buuble-sort-decreasing-order.js
@@ -0,0 +1,26 @@
+// console.log('Bubble Sort in Decreasing Order')
+
+// function bubbleSortdec(arr) {
+//     let swapped
+//     do {
+//         swapped = false
+//         for (let i = 0; i < arr.length - 1; i++) {
+//             if (arr[i] < arr[i + 1]) {
+//                 let temp = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = temp
+//                 swapped = true
+//             }
+//         }
+//     } while (swapped)
+// }
+
+// // Explanation:
+// // arr.length-1 bcz we don't need to compare the last element with anything
+// // Another loop if array has been swapped than repeate the process
+// // Just change the condition from > to <
+
+// const arrdec = [5, 3, 8, -2, 1, 4, -6, 9, 7, 0]
+
+// bubbleSortdec(arrdec)
+// console.log(arrdec)
